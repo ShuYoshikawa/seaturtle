21425d2fc2cb929102e9c8597e2fed0df34bfc4a
ShuYoshikawa <ysysyskwssss@gmail.com>
2023-05-12 22:14:09 +0900

    トップ画面を作りました

---
+++ turtle_top.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Engineer-Management</title>
+    <style>
+        div {
+            background-color: rgb(0, 200, 0);
+            color: white;
+            padding: 30px 30px 30px 30px;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div>Engineer-Management</div>
+    <br />
+    <br />
+
+    <br />
+    <br />
+    <br />
+    <br />
+
+    <button style="background-color:white;width:20%;height:60px;display:block;margin:0 auto" 
+    type="button" onclick="location.href='turtle_list.php'">一覧画面</button>
+
+    <br />
+    <br />
+    <br />
+    <button style="background-color:white;width:20%;height:60px;display:block;margin:0 auto" 
+    type="button" onclick="location.href='turtle_add.php'">登録画面</button>
+
+
+</body>
+
+</html>
